9e99ef30f9d74443ec692a54fcd90dbe6b6bc954
karth <karth@DESKTOP-RO4POV0>
2021-06-13 20:52:10 -0400

    changed to shravan

--- edu/src/edu/SwitchStatement.java
+++ edu/src/edu/SwitchStatement.java
@@ -5,7 +5,7 @@ public class SwitchStatement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-    String name = "Karthik";
+    String name = "Shravan";
     
     switch(name)
     {
