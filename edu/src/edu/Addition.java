09de968ba70d60083eb8b9b77617b746e23420eb
karth <karth@10.0.0.133>
2021-06-14 01:14:35 -0400

    new committ addded tarun name

--- edu/src/edu/Addition.java
+++ edu/src/edu/Addition.java
@@ -5,7 +5,7 @@ public class Addition {
 	public static void main (String args[]) {
 		int a=5;
 		double b=3.5;
-		double c= a--;
+		double c= a+b;
 		
 		System.out.println(c);
 		
--- edu/src/edu/SwitchStatement.java
+++ edu/src/edu/SwitchStatement.java
@@ -5,7 +5,7 @@ public class SwitchStatement {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-    String name = "Shravan";
+    String name = "Tarun";
     
     switch(name)
     {
